7258031ef105d6cadfeb778123eb7ce416042931
Davidodari <davidkibzodari@gmail.com>
2019-10-04 18:55:16 +0300

    add comment

--- src/models/GenericPlate.kt
+++ src/models/GenericPlate.kt
@@ -1,8 +1,7 @@
 package models
 
-/*
-A Generic models.Plate class acts as a container that holds some food which can be any type
-of food from things like rice to soup to meat to burgers..etc.
+/**
+ * Plate class acts as a container for any type
  */
 class Plate<T> {
     fun addFoodToPlate(vararg food: T) {}
