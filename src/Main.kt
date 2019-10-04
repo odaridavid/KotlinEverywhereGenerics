264093e0f49cdf2d2bdd94da2f379c3155d224b1
Davidodari <davidkibzodari@gmail.com>
2019-10-04 18:56:05 +0300

    add examples

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,16 @@
-import models.Fish
-import models.Plate
-import models.Ugali
+import models.*
 
 fun main() {
+
     ServingRepo.getFishPlate().addFoodToPlate(Fish(), Fish())
-    val plates: Plate<Int> = Plate()
+
+    //Takes in any type parameter
+    val plate: Plate<Int> = Plate()
+//    val plateOfFood: FoodPlate<Int> = Plate() -> Won't  Compile
+    val plateOfFood: FoodPlate<Ugali> = FoodPlate()
+
+    //Type inference from constructor
+    val saltSeasoning = Seasoning<Salt>(Salt)
+    val saltSeasoning2 = Seasoning(Salt)
+//    val plateOfFood = FoodPlate() -> Type Cant be inferred
 }
